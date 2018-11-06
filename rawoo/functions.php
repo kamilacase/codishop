2a414298e7bbdbd2db7cbd876adb1665bc9e52f2
kamilacase <xakdxshooter@live.com>
2018-11-06 16:25:18 +0100

    hook enqueue scripts

--- rawoo/functions.php
+++ rawoo/functions.php
@@ -2,8 +2,25 @@
 
 function mytheme_add_woocommerce_support() {
 	add_theme_support( 'woocommerce' );
-}
+};
 add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
 add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
 
- ?>
+
+function rawoo_enqueue_style() {
+	wp_enqueue_style( 'rawoo-style', get_stylesheet_uri());
+	wp_enqueue_style('css-fontawesome', get_template_directory_uri().'/node_modules/@fortawesome/fontawesome-free/css/all.css', false);
+	wp_enqueue_style('css-bootstrap', get_template_directory_uri().'/node_modules/bootstrap/dist/css/bootstrap.min.css', false);
+}
+
+function rawoo_enqueue_script() {
+	wp_enqueue_script( 'rawoo-script', get_stylesheet_uri());
+	wp_enqueue_script('js-jquery', get_template_directory_uri().'/node_modules/jquery/dist/jquery.min.js', false);
+	wp_enqueue_script('css-bootstrap', get_template_directory_uri().'/node_modules/bootstrap/dist/js/bootstrap.min.js', false);
+}
+
+add_action("wp_enqueue_scripts", "rawoo_enqueue_style");
+add_action("wp_enqueue_scripts", "rawoo_enqueue_script");
+
+
+ ?> 
--- rawoo/header.php
+++ rawoo/header.php
@@ -1,7 +1,6 @@
 
 <!DOCTYPE html>
 <html lang="fr">
- <?php wp_head(); ?>
 <head>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,16 +9,16 @@
 <meta name="author" content="">
 <title>Rawoo</title>
 
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
-
-<link href="<?php bloginfo(‘template_directory’);?>/blog.css" rel="stylesheet">
 
 
+
+ <?php wp_head(); ?>
 
 </head>
+<body>
 <header>
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="#">CODISHOP</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -27,24 +26,19 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#">HOME <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
+        <a class="nav-link" href="#">SHOP</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
+        <a class="nav-link" href="#">BLOG</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">ABOUT</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">CONTACT</a>
       </li>
     </ul>
     <form class="form-inline my-2 my-lg-0">
@@ -54,4 +48,3 @@
   </div>
 </nav>
 </header>
-<body>
